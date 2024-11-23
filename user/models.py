4cd78bf2fa9c3bde26f54ab93852c17e45fc04c4
FranciscoVega <pancho.vega.90@gmail.com>
2024-11-23 18:16:56 -0300

    profile form, url: /profile

--- user/models.py
+++ user/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.admin import User
 from django.dispatch import receiver
 from django.db.models.signals import post_save
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete = models.CASCADE)
     age = models.IntegerField(null=True, blank=True)
@@ -15,3 +16,5 @@ class UserProfile(models.Model):
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
         UserProfile.objects.create(user=instance)
+
+
--- user/views.py
+++ user/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .forms import UserForm, UserProfileForm
+from django.contrib.auth.decorators import login_required
+from django.db import transaction
 # Create your views here.
+
+@login_required
+@transaction.atomic
+def update_profile(request):
+    if request.method == "POST":
+        user_form = UserForm(request.POST, instance=request.user)
+        user_profile_form = UserProfileForm(request.POST, instance=request.user.userprofile)
+        if user_form.is_valid() and user_profile_form.is_valid():
+            user_form.save()
+            user_profile_form.save()
+            return redirect("user:profile")
+    else:
+        # we populate the user form 
+        user_form = UserForm(instance=request.user)
+        user_profile_form = UserProfileForm(instance=request.user.userprofile)
+    return render(request, "profile.html", {"u_form":user_form, "p_form": user_profile_form})
